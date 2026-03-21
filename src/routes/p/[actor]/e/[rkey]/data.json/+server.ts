91b1f5f76b217dd612c9a3472a5bb383088fd1f7
Florian <45694132+flo-bit@users.noreply.github.com>
2026-03-21 14:27:38 +0100

    add server route

---
+++ src/routes/p/[actor]/e/[rkey]/data.json/+server.ts
@@ -0,0 +1,36 @@
+import { json, error } from '@sveltejs/kit';
+import { getActor } from '$lib/actor';
+import { getEventRecordFromContrail } from '$lib/contrail';
+
+export async function GET({ params }) {
+	const did = await getActor(params.actor);
+
+	if (!did || !params.rkey) {
+		throw error(404, 'Event not found');
+	}
+
+	const eventRecord = await getEventRecordFromContrail({
+		did,
+		rkey: params.rkey,
+		hydrateRsvps: 50,
+		profiles: true
+	});
+
+	if (!eventRecord) {
+		throw error(404, 'Event not found');
+	}
+
+	return json(
+		{
+			...eventRecord,
+			url: `https://atmo.rsvp/p/${params.actor}/e/${params.rkey}`
+		},
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET',
+				'Cache-Control': 'public, max-age=60, s-maxage=60'
+			}
+		}
+	);
+}
