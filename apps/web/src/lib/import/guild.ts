070ae78635c51672295c41df82c64de5dcb86fc8
Willow (GHOST) <git@willow.sh>
2026-07-05 04:47:36 +0100

    fix: description is optional
    Co-authored-by: Tom Scanlan <3408465+tompscanlan@users.noreply.github.com>

--- apps/web/src/lib/import/guild.ts
+++ apps/web/src/lib/import/guild.ts
@@ -9,7 +9,7 @@ const GuildEventSchema = v.object({
 	slug: v.string(),
 	fullUrl: v.pipe(v.string(), v.url()),
 	name: v.string(),
-	description: v.string(),
+	description: v.optional(v.string()),
 	startAt: v.string(),
 	endAt: v.string(),
 	timeZone: v.string(),
